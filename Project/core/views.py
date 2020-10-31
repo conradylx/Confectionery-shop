cff1a878b329702e06a1052100bf1ee3eaf2a095
conradylx <50596942+conradylx@users.noreply.github.com>
2020-10-31 14:35:46 +0100

    Display db models in home page

--- Project/core/views.py
+++ Project/core/views.py
@@ -3,9 +3,19 @@ from django.shortcuts import render
 from .models import Item
 
 
-def item_list(request):
+def products(request):
     context = {
         "items" : Item.objects.all()
     }
+    return render(request, 'product.html', context)
 
-    return render(request, "home-page.html", context)
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def home(request):
+    context = {
+        "items": Item.objects.all()
+    }
+    return render(request, 'home.html', context)
